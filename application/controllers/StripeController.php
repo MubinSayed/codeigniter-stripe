e13b1f20fd80bda75755cccebfb7fa1f5593fe3b
Mubin <mubin@jazeerah.com>
2019-05-28 07:46:55 +0530

    removed session library

--- application/controllers/StripeController.php
+++ application/controllers/StripeController.php
@@ -11,7 +11,7 @@ class StripeController extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->load->library("session");
+        // $this->load->library("session");
         $this->load->helper('url');
         $this->load->model('transaction');
     }
